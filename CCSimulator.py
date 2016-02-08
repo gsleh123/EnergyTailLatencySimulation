d85c252e0a42e9fbbb06829daf673f41b79a3a26
Dylan Wang <dylan.wang@gmail.com>
2016-02-07 22:56:20 -0800

    Start of implementation to Jiang's model

--- CCSimulator.py
+++ CCSimulator.py
@@ -4,6 +4,7 @@ import random
 from collections import deque
 import numpy as np
 from Queue import Queue
+from pprint import pprint
 
 # plotting
 import seaborn as sns
@@ -16,19 +17,23 @@ CONTROL = False
 
 env = simpy.Environment()
 hosts = []
-num_of_hosts = 4
+num_of_hosts = 1
 
 random.seed(14)
 
 def main():
     global env
 
+    arrival_rate = 0.11
+    process_rate = 0.1
+    sleep_alpha = 0.5
+
     for i in range(num_of_hosts):
-        hosts.append(Host(i, 0.2, 0.1))
+        hosts.append(Host(i, arrival_rate, process_rate, sleep_alpha))
         env.process(hosts[-1].packet_arrival())
         env.process(hosts[-1].packet_process())
 
-    env.run(until=10000)
+    env.run(until=50000)
 
     print('done')
 
@@ -38,46 +43,30 @@ def main():
     print('%5s |%7s |%11s |%16s ' % ('Host', 'PktCnt', 'AvgLatency', 'TailLatency-p99'))
     for i in range(num_of_hosts):
 
-    	latencies = np.array(hosts[i].latencies)
-    	latencies_all.extend(hosts[i].latencies)
+        latencies = np.array(hosts[i].latencies)
+        latencies_all.extend(hosts[i].latencies)
 
         # Note: qsize is only accurate if no other threads are accessing!
-        print('%5i |%7i |%11.2f |%16.2f ' % (i, hosts[i].packet_queue.qsize(), np.mean(latencies), np.percentile(latencies, 99) ))
+        print('%5i |%7i |%11.2f |%16.2f ' % (i, hosts[i].packet_queue.qsize(), np.mean(latencies), np.percentile(latencies, 98) ))
 
     # Print totals
     print('%5s |%7i |%11.2f |%16.2f ' % ('Total', sum([host.packet_queue.qsize() for host in hosts]),
-    	np.mean(latencies_all), np.percentile(latencies_all, 99) ))
+        np.mean(latencies_all), np.percentile(latencies_all, 98) ))
 
-
+    #pprint(latencies_all)
     sns.distplot(latencies_all, norm_hist=True)
     plt.tight_layout()
     plt.show()
 
 class Host(object):
     """docstring for Host"""
-    def __init__(self, idd, arrival_rate, process_rate):
+    def __init__(self, idd, arrival_rate, process_rate, sleep_alpha):
         self.id = idd
         self.arrival_rate = arrival_rate
         self.process_rate = process_rate
+        self.sleep_alpha = sleep_alpha
         self.packet_queue = Queue()
         self.thermal_slack = ThermalSlack()
-
-        self.time_of_last_arrival = 0
-        # exponentially weighted rolling average of packet arrival rate
-        self.ewra = 0
-        # a coefficient for the ewra between 0-1
-        # higher values = more influence on the current measurement
-        # lower values = history keeps impact longer
-        # this method uses the entire history (through the current mean)
-        # but the number of effective samples is approximated as N = alpha^(-1)
-        # reference: http://stats.stackexchange.com/a/44651
-        self.ewra_alpha = 0.1
-
-        # TODO: softcode the maxlen
-        # this is a circular buffer for storing past ewra values
-        # these will be used to calculate percentiles
-        # maxlen=50 means the ewra at the last 50 arrivals will be used
-        self.ewra_samples = deque([], maxlen = 50)
 
         # latencies of all packets for this host
         self.latencies = []
@@ -91,65 +80,51 @@ class Host(object):
             self.packet_queue.put(Packet())
 
             # time since the last packet arrived
-            time_diff = env.now - self.time_of_last_arrival
-
-            # decide if we should adjust the frequency
-            # the thermal slack class will downclock if needed (due to temp)
-            
-            self.ewra = self.ewra + self.ewra_alpha*(time_diff - self.ewra)
-
-            if len(self.ewra_samples) == 0:
-                self.ewra_samples.append(self.ewra)
-                continue
-
-            if CONTROL:
-            	pass
-            else:
-	            if self.thermal_slack.freq == self.thermal_slack.base_freq:
-
-	                if self.ewra > np.percentile(self.ewra_samples, 80):
-	                    self.thermal_slack.freq = self.thermal_slack.sudden_spike_freq
-	                #elif self.ewra_samples[0:50/2]
-
-	            else: # we are at a frequency higher than base
-
-	                # if we are at less than 40th percentile, downclock
-	                if self.ewra < np.percentile(self.ewra_samples, 40):
-	                    self.thermal_slack.freq = self.thermal_slack.base_freq
-
-            self.ewra_samples.append(self.ewra)
+            #time_diff = env.now - self.time_of_last_arrival
         
     def packet_process(self):
         global env
 
         while True:
 
-        	if self.packet_queue.empty():
-        		yield env.timeout(1)
-        	else:
-	            yield env.timeout(random.expovariate(self.process_rate)/self.thermal_slack.freq)
-	            pkt = self.packet_queue.get()
+            if self.packet_queue.empty():
+                # nothing to do, keep sleeping
+                yield env.timeout(1)
+            else:
+                # If freq is 0, we need to start up
+                if self.thermal_slack.freq == 0:
+                    # force a startup time wait before continuing
+                    yield env.timeout(random.expovariate(self.sleep_alpha))
+                    self.thermal_slack.SetFreq(self.thermal_slack.lower_bound_freq)
 
-	            time_taken = env.now - pkt.creation_time
-	            self.latencies.append(time_taken)
+
+                yield env.timeout(random.expovariate(self.process_rate)/self.thermal_slack.freq)
+                pkt = self.packet_queue.get()
+
+                time_taken = env.now - pkt.creation_time
+                self.latencies.append(time_taken)
+
+                # check if queue is empty. If so, go to sleep
+                if self.packet_queue.empty():
+                    self.thermal_slack.SetFreq(0)
             
 
     def temp_tick(self):
-    	global env
+        global env
 
-    	while True:
-    		yield env.timeout(1)
+        while True:
+            yield env.timeout(1)
 
-    		if not CONTROL:
-	    		self.thermal_slack.Tick()
+            if not CONTROL:
+                self.thermal_slack.Tick()
 
 class Packet(object):
-	"""docstring for Packet"""
-	def __init__(self):
-		global env
-		
-		self.creation_time = env.now
-		
+    """docstring for Packet"""
+    def __init__(self):
+        global env
+        
+        self.creation_time = env.now
+        
 
 if __name__ == '__main__':
     main()
--- ThermalModel.py
+++ ThermalModel.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 
 class ThermalSlack(object):
 	"""Acts like a buffer for modeling the thermal slack of a core"""
@@ -7,19 +8,10 @@ class ThermalSlack(object):
 		# 0 being resting temp, 100 being max allowed temp
 		# The 0-1 scale is exponential
 		self.buffer = 0
-		self.freq = 1.5
+		self.freq = 1.2
 
-		# A master table for how fast a core heats/cools at various frequencies
-		# index is frequency, value is a rate of heating/cooling (negative values = cooling)
-		# This rate is relative to the 0-100 buffer rating per 1ms
-		self.temp_table = dict()
-		self.temp_table[1.5] = -2
-		self.temp_table[2.0] = 5
-		self.temp_table[2.5] = 15
-
-		self.base_freq = 1.5
-		self.increased_avg_freq = 2.0 # to handle increased average
-		self.sudden_spike_freq = 2.5 # to handle increased std-dev
+		self.lower_bound_freq = 1.2
+		self.higher_bound_freq = 3.0
 
 	def Tick(self):
 		""" We assume the heat generation is purely based on clock rate
@@ -28,14 +20,16 @@ class ThermalSlack(object):
 		if self.buffer == 0:
 			return
 
-		temp_to_add = temp_table[self.freq]
+		# Note: we could use a freq->temp map here instead of a formula
+		# Assuming linear growth here
+		temp_to_add = FreqToTempIncrease(self.freq)
 
 		if self.buffer + temp_to_add > 100:
 			# We cannot run this tick at this temp,
 			# since it would overflow the thermal buffer
 			# Instead run at base clock
 			self.freq = self.base_freq
-			temp_to_add = temp_table[self.base_freq]
+			temp_to_add = FreqToTempIncrease(self.freq)
 
 		# We are in a temp-reduction frequency
 		if temp_to_add < 0:
@@ -51,10 +45,9 @@ class ThermalSlack(object):
 		print('tick')
 
 	def SetFreq(self, new_freq):
-		
-		# make sure this freq exists in temp_table
-		if new_freq not in temp_table:
-			raise KeyError('No thermal entry for frequency %f' % (new_freq))
-
 		self.freq = new_freq
+
+	def FreqToTempIncrease(self, freq):
+		return np.interp(freq, [self.lower_bound_freq, self.higher_bound_freq][1,10])
+
 
